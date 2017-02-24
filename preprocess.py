ab96ed86193e98f07eb42e157b41068db7640442
katehara <medhakat95@gmail.com>
2017-02-24 20:59:45 +0530

    debugged - dicrestization working

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,25 @@
 import pandas as pd
 import numpy as np 
+import json
 from numpy import *
 from operator import itemgetter
 from collections import Counter, OrderedDict
 
 def buckets(data, bins):
-    split = np.array_split(np.sort(data), bins)
-    cutoffs = [x[-1] for x in split]
-    cutoffs = cutoffs[:-1]
-    discrete = np.digitize(data, cutoffs, right=True)
-    groups = {}
-    variance = 0
-    for i in len(data):
-    	if discrete[i] in groups:
-    		groups[discrete[i]] += [data[i]]
-    	else:
-    		groups[discrete[i]] += [data[i]]
-    for i in groups:
-    	variance += np.var(groups[i], ddof=1)
-
-    return discrete, cutoffs, variance
+	split = np.array_split(np.sort(data), bins)
+	cutoffs = [x[-1] for x in split]
+	cutoffs = cutoffs[:-1]
+	discrete = np.digitize(data, cutoffs, right=True)
+	groups = {}
+	variance = 0
+	for i in range(len(data)):
+		if discrete[i] in groups:
+			groups[discrete[i]] += [data[i]]
+		else:
+			groups[discrete[i]] = [data[i]]
+	for i in groups:
+		variance += np.var(groups[i], ddof=1)
+	return discrete, cutoffs, variance
 
 def get_split(data):
 	possible = {}
@@ -36,46 +36,48 @@ def get_split(data):
 			'c':c,
 			'v':v
 		}
-	    if v <= possible[i-1]['v']:
-	        found = False;
-	    elif (v > possible[i-1]['v']) and (not found):
-	    	return possible[i-1];
+		if v <= possible[i-1]['v']:
+			found = False;
+		elif (v > possible[i-1]['v']) and (not found):
+
+			return possible[i-1];
 
 	for i in range(1,10): 
-        difference = -(possible[i+1]['v'] - possible[i]['v'])
-        if (difference > max_diff):
+		difference = -(possible[i+1]['v'] - possible[i]['v'])
+		if (difference > max_diff):
 			max_diff = difference;
 			best_groups = i+1; 
 
 	return possible[best_groups]
 
-    
+	
 
 def discretize(col):
-	sort_col = sorted(list(map(lambda x : int(int(x/10)*10),col.dropna())))
-	total = len(sort_col)
-	pro_cnt = sorted(Counter(sort_col).items(), key=itemgetter(0))
-	probs = OrderedDict()
-	for c in pro_cnt:
-		probs[c[0]] = c[1]/total
-	
-	split_criteria = get_split(sort_col)	
-
+	round_col = col.dropna() #list(map(lambda x : int(int(x/10)*10),col.dropna()))
+	split_criteria = get_split(round_col)	
 	return split_criteria
 
 def prepare_data(df, num_cols):
 	df = pd.DataFrame(df)
 	criteria = {}
 	for col in num_cols:
-		d,c,v = discretize(df[col])
-		df[col] = d
+		split_criteria = discretize(df[col])
+		df[col] = split_criteria['d']
 		criteria[col] = {
-		'cutoff': c,
-		'variance': v
+		'cutoff': split_criteria['c'],
+		'variance': split_criteria['v']
 		}
 	return df, criteria
 
 
+#testing
+# if __name__ == '__main__':
+# 	df = pd.DataFrame(np.random.randn(50, 4))
+# 	df.columns = list('abcd')
+# 	new_df,criteria = prepare_data(df,['a','c'])
+# 	df.to_csv('df.csv', sep=',')
+# 	new_df.to_csv('new_df.csv', sep=',')
+# 	with open('cri.txt','w') as file: file.write(json.dumps(criteria))
 
 
 
