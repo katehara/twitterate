b346df1147a826cfb42c82bbb6267b2504512387
Medha Katehara <medhakat95@gmail.com>
2017-03-16 15:18:32 +0530

    analyse explained

--- analyse.py
+++ analyse.py
@@ -3,27 +3,27 @@ import pandas as pd
 from data_structures import *
 
 
-def analyse():
-	args = sys.argv[1:]
-	if not args:
+def analyse(): # find P_score for all files given as sys.args
+	args = sys.argv[1:] # read file names from sys args
+	if not args: # print usage & return if file names not given
 		print('Usage: python analyse.py path/to/file.csv >:(')
 		return
 
-	for file in args:
-		if file[-4:] != '.csv':	
+	for file in args: # for each file in args
+		if file[-4:] != '.csv':	# skip files not in csv format
 			print('Only csv files accepted. Skiping - '+file+' :(')
-		elif not os.path.isfile(file):
+		elif not os.path.isfile(file): # notify if file doesn't exist
 			print(file+' doesn\'t exist. Check for typos. :/')
-		else:
-			data = pd.read_csv(file)
-			root = Node('R', data, 0, None)
-			groups = data.groupby(['partition'])
+		else: # if file is csv and exists calculate P_score and print
+			data = pd.read_csv(file) #read csv
+			root = Node('R', data, 0, None) # create root node
+			groups = data.groupby(['partition']) # group data by partition
 			parts = []
-			for gr in groups:
-				parts += [Node(gr[0], gr[1], 1, root)]
+			for gr in groups: # for all partitons
+				parts += [Node(gr[0], gr[1], 1, root)] # make nodes - for category utility
 
-			ps = sum(p.cat_utility for p in parts)/len(parts)
-			print(file + '\t\t P_Score: ' + str(ps) + ' :)')
+			ps = sum(p.cat_utility for p in parts)/len(parts) # calulate P_score for the partition
+			print(file + '\t\t P_Score: ' + str(ps) + ' :)') # give results
 	return
 
 if __name__ == "__main__":
